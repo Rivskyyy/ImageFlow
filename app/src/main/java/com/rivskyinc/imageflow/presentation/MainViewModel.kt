741abbaf73db4c4d97a69f20f79212df2a7da7ab
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-10-08 20:27:05 +0300

    getLatestPhoto added to MainViewModel

--- app/src/main/java/com/rivskyinc/imageflow/presentation/MainViewModel.kt
+++ app/src/main/java/com/rivskyinc/imageflow/presentation/MainViewModel.kt
@@ -2,8 +2,14 @@ package com.rivskyinc.imageflow.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bumptech.glide.load.HttpException
 import com.rivskyinc.imageflow.domain.GetImageUseCase
 import com.rivskyinc.imageflow.domain.entities.Image
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
 
 import javax.inject.Inject
 
@@ -11,5 +17,24 @@ class MainViewModel @Inject  constructor(private val getImageUseCase: GetImageUs
 
     private var imageList= MutableLiveData<Image>()
 
+    init {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                getLatestPhoto()
+            }
+        }
+
+    }
+
+    private suspend fun getLatestPhoto(){
+        try {
+            val data = getImageUseCase.invoke()
+            imageList.postValue(data)
+        } catch (e: HttpException) {
+            throw RuntimeException("HttpException : $e ")
+        } catch (e: IOException) {
+            throw RuntimeException("IOException : $e ")
+        }
+    }
 
 }
