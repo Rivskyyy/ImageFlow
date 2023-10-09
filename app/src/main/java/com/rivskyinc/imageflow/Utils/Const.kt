beb2f782c00929dba53a90622e7547b21bb5752e
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-10-09 08:16:58 +0300

    changed BASE_URL in Const

--- app/src/main/java/com/rivskyinc/imageflow/Utils/Const.kt
+++ app/src/main/java/com/rivskyinc/imageflow/Utils/Const.kt
@@ -1,5 +1,7 @@
 package com.rivskyinc.imageflow.Utils
 
 object Const {
-    const val BASE_URL = "https://api.flickr.com/services/"
+//    const val BASE_URL = "https://api.flickr.com/services/"
+    const val BASE_URL = "https://www.flickr.com/services/"
+
 }
