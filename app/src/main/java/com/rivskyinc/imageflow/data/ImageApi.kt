874f1f2045d2b7b74bc7a21b85a8370484daf211
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-10-09 08:15:41 +0300

    added imageGetInfo to ImageApi

--- app/src/main/java/com/rivskyinc/imageflow/data/ImageApi.kt
+++ app/src/main/java/com/rivskyinc/imageflow/data/ImageApi.kt
@@ -2,12 +2,20 @@ package com.rivskyinc.imageflow.data
 
 import com.rivskyinc.imageflow.Utils.API_KEY
 import com.rivskyinc.imageflow.domain.entities.Image
-import retrofit2.http.GET
+import com.rivskyinc.imageflow.domain.entities.PhotoX.Result
 import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ImageApi {
 
     @GET("rest/?method=flickr.interestingness.getList&api_key=$API_KEY&format=json&nojsoncallback=1") // <- put your API_KEY here
     suspend fun imageResponse() : Response<Image?>
+
+    @GET("rest/?method=flickr.photos.getInfo&api_key=$API_KEY&format=json&nojsoncallback=1")
+    suspend fun imageGetInfo(
+        @Query("photo_id")
+        id : String
+    ) : Response<Result?>
 
 }
