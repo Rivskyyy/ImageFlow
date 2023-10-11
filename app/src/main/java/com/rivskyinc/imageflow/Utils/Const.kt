2dbefa9cd3c6732c5ede4f836db0b2052c709190
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-10-11 06:37:45 +0300

    BASE_IMAGE_URL added to Const.kt

--- app/src/main/java/com/rivskyinc/imageflow/Utils/Const.kt
+++ app/src/main/java/com/rivskyinc/imageflow/Utils/Const.kt
@@ -3,5 +3,5 @@ package com.rivskyinc.imageflow.Utils
 object Const {
 //    const val BASE_URL = "https://api.flickr.com/services/"
     const val BASE_URL = "https://www.flickr.com/services/"
-
+    const val BASE_IMAGE_URL = "https://live.staticflickr.com/"
 }
